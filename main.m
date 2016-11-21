bbb63e2a827f869e5fd845bc8e23915bb47837f2
George Chapman <george.r.chapman@gmail.com>
2016-11-21 13:24:36 +0000

    added variable for pathname of home dir

--- main.m
+++ main.m
@@ -1,21 +1,27 @@
 % progress: saves and reads in individual frames of input image.  need to
 % add selecting of droplet in first frame.
 
+homeDir = 'C:\Users\dwh\Documents\MATLAB\MQMTracking';
+dirSlash = '\';
+
+% homeDir = ['/Users/georgechapman/Documents/OneDrive/Documents/',...
+%     'University/Year 4/Masters Project/Matlab'];
+% dirSlash = '/';
 
 clear
-cd(['C:\Users\dwh\Documents\MATLAB\MQMTracking']);
+cd(homeDir);
 
 %% Load video
 % loads video file and converts into a matrix of images.
 videoFile = 'corral1';
 videoExt = '.avi';
-nframes = save_frames(videoFile,videoExt);
+nframes = save_frames(videoFile,videoExt, dirSlash);
 
 %% Find coordinates
 % finds the coordinates in the video of the maximum change in pixels
 % between frames.
 
-cd(['C:\Users\dwh\Documents\MATLAB\MQMTracking\frames\',videoFile])
+cd([homeDir, dirSlash, 'frames', dirSlash, videoFile])
 
 threshold = 0; % min difference between frames to be considered
 maxShift = 20; % circle of radius 10 for search for next position
@@ -71,27 +77,27 @@ close(h);
 xMax(1) = xMax(2);
 yMax(1) = yMax(2);
 
-cd(['C:\Users\dwh\Documents\MATLAB\MQMTracking']);
+cd(homeDir);
 
 %% Plot results
-cd(['C:\Users\dwh\Documents\MATLAB\MQMTracking\frames\',videoFile])
+cd([homeDir, dirSlash, 'frames', dirSlash, videoFile])
 % animated view
-for i = 2:nframes
-    load([num2str(frames(i)),'.mat']);
-    imagesc(frame);
-    hold on;
-    plot(xMax(1:i),yMax(1:i),'r-')
-    hold off;
-    axis equal
-    pause;
-end
+% for i = 2:nframes
+%     load([num2str(frames(i)),'.mat']);
+%     imagesc(frame);
+%     hold on;
+%     plot(xMax(1:i),yMax(1:i),'r-')
+%     hold off;
+%     axis equal
+%     pause;
+% end
 
 % end view
-% load([num2str(frames(end)),'.mat']);
-% imagesc(frame);
-% hold on;
-% plot(xMax,yMax,'r-')
-% hold off;
-% axis equal
+load([num2str(frames(end)),'.mat']);
+imagesc(frame);
+hold on;
+plot(xMax,yMax,'r-')
+hold off;
+axis equal
 
-cd(['C:\Users\dwh\Documents\MATLAB\MQMTracking']);
+cd(homeDir);
--- save_frames.m
+++ save_frames.m
@@ -1,19 +1,19 @@
-function n = save_frames(videoFile, videoExt)
+function n = save_frames(videoFile, videoExt, slash)
 
 %% Load video
 % loads video file and converts into a matrix of images.
 v = VideoReader([videoFile, videoExt]);
 
-foldercheck = dir(['frames\', videoFile]);
+foldercheck = dir(['frames', slash, videoFile]);
 if isempty(foldercheck)
-    mkdir(['frames\', videoFile]);
+    mkdir(['frames', slash, videoFile]);
 end
 
 count = 0;
 while hasFrame(v)
     count = count + 1;
     frame = rgb2gray(readFrame(v)); % grayscale of image
-    save(['frames\', videoFile, '\', int2str(count), '.mat'],'frame','-v7.3') % save frames
+    save(['frames', slash , videoFile, slash, int2str(count), '.mat'],'frame','-v7.3') % save frames
 end
 
 n = count;
